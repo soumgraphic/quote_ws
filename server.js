e5881329d311dbe5bf39f5bd96b63e81f7be70b3
Soumaila A DIARRA <soumgraphic@gmail.com>
2018-11-30 12:32:41 +0100

    commit second test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ('express'),
     app = express (),
     bodyParser = require('body-parser');
-port = process.env.PORT || 3000;
+port = process.env.PORT || 8080;
 global.config = require('./config/config');
 
 //Parse l'application/json
